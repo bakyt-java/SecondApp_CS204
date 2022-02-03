ae4a6f6fc3fdaa95f924ae3c24ba880b37411c22
Hello World app <bakyt.ikramov@iaau.edu.kg>
2022-02-03 23:27:37 +0600

    Initial commit

---
+++ src/main/java/baha/edu/kg/SecondApp_CS204/SecondAppCs204Application.java
@@ -0,0 +1,26 @@
+package baha.edu.kg.SecondApp_CS204;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class SecondAppCs204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondAppCs204Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String index () {
+		return "How Are Yoy!";
+	}
+
+
+
+}
---
+++ src/test/java/baha/edu/kg/SecondApp_CS204/SecondAppCs204ApplicationTests.java
@@ -0,0 +1,13 @@
+package baha.edu.kg.SecondApp_CS204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondAppCs204ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
